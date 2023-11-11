8150d2eec3e1b502c9aeaf59e06de4a172ab5060
Matej Galic <matej.galic2013@gmail.com>
2023-11-11 19:53:06 +0100

    Dovršena Main stranica

--- IzvorniKod/frontend/src/pages/MainScreen.js
+++ IzvorniKod/frontend/src/pages/MainScreen.js
@@ -1,21 +1,42 @@
+import { Link } from "react-router-dom";
+import styles from "./MainScreen.module.css";
+
 export default function MainScreen() {
   return (
-    <div>
-      <Logo />
-      <SearchBar />
-      <Profile />
-    </div>
+    <>
+      <div className={styles.div}>
+        <Logo />
+        <SearchBar />
+        <Profile />
+      </div>
+      <hr />
+      <div className={styles.picturePart}></div>
+    </>
   );
 }
 
 function Logo() {
-  return <div></div>;
+  return <div className={styles.divLogo}>Flip Memo</div>;
 }
 
 function SearchBar() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <input
+          className={styles.inputSearch}
+          type="text"
+          placeholder="Search..."
+        ></input>
+      </form>
+    </div>
+  );
 }
 
 function Profile() {
-  return <div></div>;
+  return (
+    <Link to="/profileSettings" className={styles.profileSettings}>
+      Profile
+    </Link>
+  );
 }
