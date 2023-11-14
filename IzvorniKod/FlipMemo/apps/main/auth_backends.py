11477590961b9dd2681b746d9146ce5e6d14ae0c
KristijanMilic <km54331@fer.hr>
2023-11-14 18:00:46 +0100

    Fix za migracije

--- IzvorniKod/FlipMemo/apps/main/auth_backends.py
+++ IzvorniKod/FlipMemo/apps/main/auth_backends.py
@@ -3,9 +3,6 @@ from .models import CustomUser
 
 class FlipMemoAuthBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
-        if kwargs.get('username') is not None and email is None:
-            email = kwargs.get('username')
-
         user = CustomUser.objects.get(email=email)
         if user.check_password(password):
             return user
