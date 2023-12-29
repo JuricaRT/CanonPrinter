b37df6fea6f298c997844b4b6e81a610cb494a4f
Jurica Runtas <jurica.runtas@fer.hr>
2023-12-29 09:28:38 +0100

    maknut dupliciran kod sa main screena

--- IzvorniKod/frontend/src/containers/ModifyDictionaries.js
+++ IzvorniKod/frontend/src/containers/ModifyDictionaries.js
@@ -1,41 +1,17 @@
-import React, { useState, useEffect, useSyncExternalStore } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, GlobalStyle } from "../elements/global";
 import { connect } from "react-redux";
 import Banner from "./Banner";
 import { useNavigate } from "react-router-dom";
 import * as Element from "../elements/modifyDictionaries";
-import List from "../components/List";
 import { add_word_to_dictionary } from "../actions/admin";
-import {
-  get_dictionaries,
-  select_dictionary,
-  select_language,
-} from "../actions/learningSpecs";
 import { create_dictionary } from "../actions/admin";
 
 const ModifyDictionaries = ({
   isAuthenticated,
-  dictionaries,
-  uniqueLang,
-  selected_dictionary,
-  selected_mode,
-  selected_language,
-  get_dictionaries,
-  select_language,
-  select_dictionary,
   create_dictionary,
+  add_word_to_dictionary
 }) => {
-  //matejev dio
-  const [displayLearning, setDisplayLearning] = useState(true);
-  const [displayLanguages, setDisplayLanguages] = useState(false);
-  const [displayDictionaries, setDisplayDictionaries] = useState(false);
-  const [displayModes, setDisplayModes] = useState(false);
-  const [languages, setLanguages] = useState(null);
-
-  const [selectedLanguage, setSelectedLanguage] = useState(null);
-  const [selectedDictionary, setSelectedDictionary] = useState(null);
-
-  //moj dio
   const [dictionaryName, setDictionaryName] = useState("");
   const [addDictionaries, setAddDictionaries] = useState(false);
   const [language, setLanguage] = useState("");
@@ -52,11 +28,9 @@ const ModifyDictionaries = ({
 
   const navigate = useNavigate();
   useEffect(() => {
-    get_dictionaries();
     if (!isAuthenticated || isAuthenticated === null) navigate("/");
-  }, [isAuthenticated, navigate, get_dictionaries]);
-
-  //moj dio
+  }, [isAuthenticated, navigate]);
+  
   function createDictionary() {
     setAddDictionaries(!addDictionaries);
   }
@@ -67,11 +41,6 @@ const ModifyDictionaries = ({
 
   function changeLanguageName(change) {
     setLanguage(change.target.value);
-  }
-
-  function submitDictionary() {
-    setAddDictionaries(false);
-    create_dictionary(dictionaryName, language);
   }
 
   function changeDictionaryChanges() {
@@ -112,55 +81,25 @@ const ModifyDictionaries = ({
     setWordLanguage(change.target.value);
   }
 
-  //matejev dio
-
-  useEffect(() => {
-    setSelectedLanguage(selected_language);
-    setSelectedDictionary(selected_dictionary);
-  }, [selected_language, selected_dictionary, selected_mode]);
-
-  useEffect(() => {
-    if (selectedLanguage !== null) {
-      setDisplayLanguages(false);
-      setDisplayDictionaries(true);
-    }
-
-    if (selectedDictionary !== null) {
-      setDisplayDictionaries(false);
-      setDisplayModes(true);
-    }
-  }, [selectedLanguage, selectedDictionary]);
-
-  function customizeLearning() {
-    setDisplayLearning(false);
-    setLanguages(uniqueLang);
-    setDisplayLanguages(true);
-  }
-
-  function dictionaryBack() {
-    setDisplayDictionaries(false);
-    setDisplayLanguages(true);
-    select_language(null);
-  }
-
-  function modeBack() {
-    setDisplayModes(false);
-    setDisplayDictionaries(true);
-    select_dictionary(null);
-  }
-
   function submitWord() {
     if (currentAddWordButtonAction === "Add") {
-      //setDictionaryChanges(false)
+      setDictionaryChanges(false)
+      /*
       add_word_to_dictionary(
-        selectedDictionary,
+        "temp",
+        //selectedDictionary,
         word,
         wordLanguage,
         translation,
         wordType,
         definition
-      );
+      );*/
     }
+  }
+
+  function submitDictionary() {
+    setAddDictionaries(false);
+    create_dictionary(dictionaryName, language);
   }
 
   return (
@@ -189,38 +128,13 @@ const ModifyDictionaries = ({
             </Element.SubmitButtonForAddingDictionary>
           </>
         )}
+
         <Element.ChangeDictionaryButton onClick={changeDictionaryChanges}>
           Add/remove words
         </Element.ChangeDictionaryButton>
-        {dictionaryChanges && (
+
+        { dictionaryChanges && (
           <>
-            {/* matejev dio */}
-            {displayLearning && (
-              <Element.LearningStart onClick={customizeLearning}>
-                Customize learning
-              </Element.LearningStart>
-            )}
-            {displayLanguages && (
-              <Element.LanguageSelect>
-                Select language
-                <List elements={languages} type="lang" />
-              </Element.LanguageSelect>
-            )}
-            {displayDictionaries && (
-              <Element.DictionarySelect>
-                Select dictionary
-                <List elements={dictionaries[selectedLanguage]} type="dict" />
-                <Element.DictionaryBack onClick={dictionaryBack}>
-                  &larr;
-                </Element.DictionaryBack>
-              </Element.DictionarySelect>
-            )}
-            {displayModes && (
-              <Element.ModeSelect>
-                <Element.ModeBack onClick={modeBack}>&larr;</Element.ModeBack>
-              </Element.ModeSelect>
-            )}
-            {/* moj dio */}
             <Element.WordAdditionInput
               type="text"
               placeholder="Word..."
@@ -271,7 +185,7 @@ const ModifyDictionaries = ({
               </Element.WordAdditionButton>
             </Element.WordChangesDiv>
           </>
-        )}
+          )}
       </Container>
     </React.Fragment>
   );
@@ -280,17 +194,9 @@ const ModifyDictionaries = ({
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   is_superuser: state.profile.is_admin,
-  dictionaries: state.learningSpecsReducer.dictionaries,
-  uniqueLang: state.learningSpecsReducer.uniqueLang,
-  selected_dictionary: state.learningSpecsReducer.selectedDictionary,
-  selected_mode: state.learningSpecsReducer.selectedMode,
-  selected_language: state.learningSpecsReducer.language,
 });
 
 export default connect(mapStateToProps, {
-  get_dictionaries,
-  select_dictionary,
-  select_language,
   create_dictionary,
   add_word_to_dictionary,
 })(ModifyDictionaries);
